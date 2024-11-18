628422dc8486d57b15b931b2715f81abb243d0e4
Abhishek Pandit <apandit3@kent.edu>
2024-11-18 17:09:57 -0500

    code for batch file added

--- logAppend.cpp
+++ logAppend.cpp
@@ -10,37 +10,12 @@
 #include <vector>
 
 
-using namespace std;
-
-
-// void printHelp() {
-//     cout << 
-// 	"\t-T timestamp Time the event is recorded. This timestamp is formatted as the number of seconds since the gallery opened and is a non-negative integer (ranging from 1 to 1073741823 inclusively). Time should always increase, invoking logappend with an event at a time that is prior to the most recent event already recorded is an error.\n\n"
-
-// 	"\t-K token Token used to authenticate the log. This token consists of an arbitrary-sized string of alphanumeric (a-z, A-Z, and 0-9) characters. The user supplies this token via the command line when creating a new log. Once a log is created with a specific token, any subsequent appends to that log must use the same token. You may assume the token contains sufficient entropy that it cannot be guessed in a reasonable amount of time.\n\n"
-
-// 	"\t-E employee-name Name of employee. Names are alphabetic characters (a-z, A-Z) in upper and lower case. Names may not contain spaces. Names are case sensitive. Employees and guests can have the same name. The names can be arbitrarily long. More concretely, if you make an assumption about lengths, and an attacker can successfully land a correctness, or even confidentiality or integrity attack on the contest server by using a longer name, this will be considered a valid attack.\n\n"
-
-// 	"\t-G guest-name Name of guest. Names are alphabetic characters (a-z, A-Z) in upper and lower case. Names may not contain spaces. Names are case sensitive. Employees and guests can have the same name. The names can be arbitrarily long. More concretely, if you make an assumption about lengths, and an attacker can successfully land a correctness, or even confidentiality or integrity attack on the contest server by using a longer name, this will be considered a valid attack.\n\n"
-
-// 	"\t-A Specify that the current event is an arrival; can be used with -E, -G, and -R. This option can be used to signify the arrival of an employee or guest to the gallery, or, to a specific room with -R. If -R is not provided, -A indicates an arrival to the gallery as a whole. No employee or guest should enter a room without first entering the gallery. No employee or guest should enter a room without having left a previous room. Violation of either of these conditions implies inconsistency with the current log state and should result in logappend exiting with an error condition.\n\n"
-
-// 	"\t-L Specify that the current event is a departure, can be used with -E, -G, and -R.This option can be used to signify the departure of an employee or guest from the gallery, or, from a specific room with -R. If -R is not provided, -L indicates a deparature from the gallery as a whole. No employee or guest should leave the gallery without first leaving the last room they entered. No employee or guest should leave a room without entering it. Violation of either of these conditions implies inconsistency with the current log state and should result in logappend exiting with an error condition.\n\n"
-
-// 	"\t-R room-id Specifies the room ID for an event. Room IDs are non-negative integer characters with no spaces (ranging from 0 to 1073741823 inclusively). Leading zeros in room IDs should be dropped, such that 003, 03, and 3 are all equivalent room IDs. A gallery is composed of multiple rooms. A complete list of the rooms of the gallery is not available and rooms will only be described when an employee or guest enters or leaves one. A room cannot be left by an employee or guest unless that employee or guest has previously entered that room. An employee or guest may only occupy one room at a time. If a room ID is not specified, the event is for the entire art gallery.\n\n"
-
-// 	"\tlog The name of the file containing the event log. The log’s filename may be specified with a string of alphanumeric characters (including underscores and periods). If the log does not exist, logappend should create it. logappend should add data to the log, preserving the history of the log such that queries from logread can be answered. If the log file cannot be created due to an invalid name, or any other error, logappend should print “invalid” and return 255.\n\n"
-
-// 	"\t-B file Specifies a batch file of commands. file contains one or more command lines, not including the logappend command itself (just its options); the lines are separated by \n (newlines), but the final newline in a file is optional. These commands should be processed by logappend individually, in order. This allows logappend to add data to the file without forking or re-invoking. Of course, option -B cannot itself appear in one of these command lines. Commands specified in a batch file include the log name. If a single line in a batch file is invalid, print the appropriate error message for that line and continue processing the rest of the batch file. Here is an example (the last one).\n\n";
-// }
-
-
 void check_command(int argc, char *argv[]){
 	if (argc == 3){
 		assert(
 			std::strcmp(argv[1], "-B") == 0	
 		);
-		std::cout << "Success" << std::endl;
+		std::cout << "-B check success" << std::endl;
 	}
 	if (argc == 9){
 		assert(
@@ -63,6 +38,17 @@ void check_command(int argc, char *argv[]){
 
 	}
 
+}
+
+std::string batch_validation(const std::string str){
+	for (char c: str){
+		if(!std::isalnum(c)){
+			std::cerr << "Invalid: Batch" << std::endl;
+			exit(255);
+		}
+	}
+	std::cout << "Batch is successfully validated!" << std::endl;
+	return str;
 }
 
 int get_most_recent_time(int argc, std::string filename){
@@ -237,40 +223,35 @@ bool check_constraints(char *argv[],std::string filename){
 		exit(255);
 	}
 	if (std::equal(lastList.begin() + 2, lastList.end(), inputList.begin() + 3)){
-		std::cout << "Already in the room" << std::endl;
+		if (lastList.size() == 8){
+			std::cout << "Already in the gallery" << std::endl;
+		}else if(lastList.size() == 10){
+			std::cout << "Already in the room" << std::endl;
+
+		}
+		
 		exit(255);
 	}
 	return false;
 
 }
-int main(int argc, char *argv[]) {
+
+void executor(int argc, char *argv[]){
 	std::string filename;
-    
-   
-	
 	std::cout << argc<< std::endl;
-	if (argc == 3 || argc == 9 || argc == 11){
+	if (argc == 9 || argc == 11){
 		std::cout << "successful" << std::endl;
 		// all the leftover code goes here
 		// check if it is 9, 3, or 11
-		if (argc == 3)
-		{
-		
-			std::cout << "It is a batch file" << std::endl;
-			/* code */
-
-		}
-		else if(argc == 9) {
+		if(argc == 9) {
 			check_command(argc, argv);
 			filename = file_validation(argv[8]) + ".txt";
 			int time = validate_timestamp(argc, std::stoi(argv[2]), filename);
-
 			std::string token = token_validation(argv[4]);
 			std::string name = name_validation(argv[7]);
 			check_constraints(argv, filename);
 			
 			std::cout << "It is a single command with no room in it." << std::endl;
-
 
 		}else if(argc == 11){
 			check_command(argc, argv);
@@ -328,5 +309,63 @@ int main(int argc, char *argv[]) {
 	// }
 
 
+
+}
+
+void process_single_command(const std::string& command) {
+    if (command.empty() || command.find("-B") != std::string::npos) {
+        std::cerr << "Invalid command: " << command << std::endl;
+        return;
+    }
+
+    // Split command into arguments (argc and argv format)
+    std::istringstream iss(command);
+    std::vector<std::string> args;
+    std::string arg;
+	args.push_back("logappend");
+    while (iss >> arg) {
+        args.push_back(arg);
+    }
+
+    // Prepare argc and argv for the executor
+    int argc = args.size();
+    std::vector<char*> argv(argc);
+
+    for (size_t i = 0; i < args.size(); ++i) {
+        argv[i] = const_cast<char*>(args[i].c_str());
+    }
+
+    // Call the executor function
+	std::cout << "the executor" << std::endl;
+	for (int i = 0; i < argc; ++i){
+		std::cout << argv[i] << " ";
+	}
+    executor(argc, argv.data());
+}
+
+
+int main(int argc, char *argv[]) {
+	if (argc == 3){
+		// run the batch command
+		check_command(argc, argv);
+		std::string filename = batch_validation(argv[2]) + ".txt";
+		std::cout << filename << std::endl;
+
+		std::fstream file(filename);
+		if(!file.is_open()){
+			std::cout << "Error opening the batch file!" << std::endl;
+			exit(255); 
+		}
+		std::string line;
+		while(std::getline(file, line)){
+			process_single_command(line);
+		}
+		file.close();
+		
+
+	}else{
+		executor(argc, argv);
+	}
+	
 }
 
