020b9b0c62ba5b4199e1d7e5c3b2f7195e416d46
Nick <ngstafford1@gmail.com>
2024-11-19 21:58:40 -0500

    updated crypto and gitignore

--- include/crypto.h
+++ include/crypto.h
@@ -1,8 +1,7 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
-#include <openssl/aes.h>
-#include <openssl/rand.h>
+#include <openssl/evp.h>  // Include EVP functions
 #include <vector>
 #include <string>
 
@@ -11,13 +10,8 @@ public:
     static const int AES_KEY_LENGTH = 256; // AES key length (in bits)
     static const int MY_AES_BLOCK_SIZE = 16; // AES block size (in bytes)
 
-    // Generates a random AES key of the specified length
     static std::vector<unsigned char> generateKey();
-
-    // Encrypts plaintext using AES encryption
     static std::vector<unsigned char> encrypt(const std::string& plainText, const std::vector<unsigned char>& key);
-
-    // Decrypts ciphertext using AES decryption
     static std::string decrypt(const std::vector<unsigned char>& cipherText, const std::vector<unsigned char>& key);
 };
 
