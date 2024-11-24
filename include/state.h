d6a66cb7b8c17ec743b4245e52643a2a230b581b
Nicholas Stafford <ngstafford1@gmail.com>
2024-11-24 02:38:01 -0500

    made a more rare delimiter

--- include/state.h
+++ include/state.h
@@ -16,23 +16,21 @@
 #include "utils.h"
 // Enum to represent the participant type (Employee or Guest)
 
+// Event class to represent each event
 class Event {
 public:
   int timestamp;
   std::string person;
   int from_location;
   int to_location;
-  ParticipantType
-      participantType; // Whether the event is for an employee or guest
+  ParticipantType participantType;
 
-  // Constructor to initialize the event
+  static const char DELIMITER;
+
   Event(int ts, const std::string &p, int from, int to, ParticipantType type)
       : timestamp(ts), person(p), from_location(from), to_location(to),
-        participantType(type) {
-    std::cout << "EVENT CREATED WITH PERSON: " << person << std::endl;
-  }
+        participantType(type) {}
 
-  // Method to print the event details
   void printEvent(std::string s = "\t  ") const {
     std::cout << s << "Timestamp: " << timestamp << ", Person: \"" << person
               << "\", From Location: " << from_location
@@ -41,33 +39,33 @@ public:
                                                                : "Guest")
               << std::endl;
   }
-  // Serialize event to string
+
   std::string serialize() const {
     std::stringstream ss;
-    ss << timestamp << "," << person << "," << from_location << ","
-       << to_location << "," << static_cast<int>(participantType);
+    ss << timestamp << DELIMITER << person << DELIMITER << from_location
+       << DELIMITER << to_location << DELIMITER
+       << static_cast<int>(participantType);
     return ss.str();
   }
 
-  // Deserialize string to Event
   static Event deserialize(const std::string &data) {
     std::stringstream ss(data);
     int ts, from, to, typeInt;
     std::string person;
-    char comma;
+    std::string temp;
 
-    // Read timestamp, expecting an integer followed by a comma
-    ss >> ts >> comma;
+    std::getline(ss, temp, DELIMITER);
+    ts = std::stoi(temp);
 
-    // Read the 'person' field which could contain commas (so we need to read
-    // until the next comma)
-    std::getline(ss, person, ',');
+    std::getline(ss, person, DELIMITER);
+    std::getline(ss, temp, DELIMITER);
+    from = std::stoi(temp);
 
-    // Read from_location and to_location (integers)
-    ss >> from >> comma >> to >> comma;
+    std::getline(ss, temp, DELIMITER);
+    to = std::stoi(temp);
 
-    // Read participant type (integer value)
-    ss >> typeInt;
+    std::getline(ss, temp);
+    typeInt = std::stoi(temp);
 
     // Return a newly constructed Event object using the parsed data
     return Event(ts, person, from, to, static_cast<ParticipantType>(typeInt));
--- src/state.cpp
+++ src/state.cpp
@@ -1,5 +1,4 @@
-#include <string>
-
 #include "state.h"
 
 const std::string Gallery::LOG_DIR = "logs/";
+const char Event::DELIMITER = ';';
